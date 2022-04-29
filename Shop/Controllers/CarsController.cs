8a5d9a853394cc6e7a5ccb7ed7ae6231bfb619b8
YevheniiMelnyk <melnyk.evgenii.o@gmail.com>
2022-04-29 15:18:06 +0300

    Commit

---
+++ Shop/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCategories)
+        {
+            _allCars = iAllCars;
+            _allCategories = iCarsCategories;
+        }
+        public ViewResult GoodsList()
+        {
+            var cars = _allCars.Cars;
+            return View(cars);
+        }
+    }
+}
---
+++ Shop/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Shop.Data.Interfaces;
+using Shop.Data.Mocks;
+namespace Shop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddTransient<IAllCars, MockCars>();
+            services.AddTransient<ICarsCategory, MockCategory>();
+        }
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
